a23d1f90571afdda1106509f2ae5009d6c7e927f
Natasha Ursipuny <natashaursipuny01@gmail.com>
2021-11-22 06:08:13 -0800

    Update 22-11-2021

--- movie8/js/tmdb.js
+++ movie8/js/tmdb.js
@@ -1,33 +1,34 @@
-const key = '46afac3017cd5cb3c8a9f3704644497d';  //sesuai API Key Anda 
+const key = '46afac3017cd5cb3c8a9f3704644497d';   
 const endpoint_url = 'https://api.themoviedb.org/3/';
 
-function getListMovie() {
-    fetch(endpoint_url + "movie/now_playing?api_key=" + key + "&language=en-US&page=1")
+function getListMovie(services, sectiontitle) {
+    fetch(endpoint_url + services + "?api_key=" + key + "&language=en-US&page=1")
     .then(status)
     .then(json)
     .then(function(data) {
-    // Objek/array JavaScript dari response.json() masuk lewat data.
-    // Menyusun komponen card movie secara dinamis
 
     var moviesHTML = "";
     data.results.forEach(function(movie) {
-    moviesHTML += `
+    moviesHTML =  moviesHTML + `
         <div class="col m3 s6">
-            <div class="card">
+            <div class="card moviecard">
                 <a href="./movie.html?id=${movie.id}">
                     <div class="card-image waves-effect waves-block waves-light">
                         <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" />
                     </div>
                 </a>
                 <div class="card-content text-center">
-                    <strong>${movie.title}</strong>
+                    <strong>${movie.title}</strong><br>
+                    <strong>Release Date : ${movie.release_date}</strong>
                 </div>
+
             </div>
         </div>
         `;
     });
 
     document.getElementById("movie_list").innerHTML = moviesHTML;
+    document.getElementById("section_title").innerHTML = sectiontitle;
     })
     .catch(error);
 }
